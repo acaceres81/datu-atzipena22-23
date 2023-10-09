e9adae26b6b9d6f7a3e77d6f90bd4e13b71f5a80
acaceres81 <114486260+acaceres81@users.noreply.github.com>
2023-10-09 09:03:42 +0200

    Create jsonfiles programme

---
+++ 3-Json fitxategiak/jsonfiles/src/main/java/dambi/JsonZuhaitzaSortuBi.java
@@ -0,0 +1,43 @@
+package dambi;
+
+import javax.json.JsonValue;
+import javax.json.JsonObject;
+import javax.json.JsonArray;
+import javax.json.JsonNumber;
+import javax.json.JsonString;
+
+
+public class JsonZuhaitzaSortuBi {
+    public static void navigateTree(JsonValue tree, String key) {
+        if (key != null)
+           System.out.print("Key " + key + ": ");
+        switch(tree.getValueType()) {
+           case OBJECT:
+              System.out.println("OBJECT");
+              JsonObject object = (JsonObject) tree;
+              for (String name : object.keySet())
+                 navigateTree(object.get(name), name);
+              break;
+           case ARRAY:
+              System.out.println("ARRAY");
+              JsonArray array = (JsonArray) tree;
+              for (JsonValue val : array)
+                 navigateTree(val, null);
+              break;
+           case STRING:
+              JsonString st = (JsonString) tree;
+              System.out.println("STRING " + st.getString());
+              break;
+           case NUMBER:
+              JsonNumber num = (JsonNumber) tree;
+              System.out.println("NUMBER " + num.toString());
+              break;
+           case TRUE:
+           case FALSE:
+           case NULL:
+              System.out.println(tree.getValueType().toString());
+              break;
+        }
+     }
+    
+}
