bbd437d92dfe403496937b0b6f528c365cb53812
acaceres81 <114486260+acaceres81@users.noreply.github.com>
2023-12-05 14:19:47 +0100

    05/12/2023

--- 4- Spring/AccesingRelationalData/src/main/java/dambi/AccesingRelationalData/MainController.java
+++ 4- Spring/AccesingRelationalData/src/main/java/dambi/AccesingRelationalData/MainController.java
@@ -1,14 +1,22 @@
 package dambi.AccesingRelationalData;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller // This means that this class is a Controller
+import jakarta.validation.Valid;
+
+@RestController // This means that this class is a Controller
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
 public class MainController {
   @Autowired // This means to get the bean called userRepository
@@ -33,4 +41,30 @@ public class MainController {
     // This returns a JSON or XML with the users
     return userRepository.findAll();
   }
+
+  @PutMapping(value = "/update/{userId}")
+	public ResponseEntity updateContact(@Valid @RequestBody User user, @PathVariable int userId) {
+		try {
+			user.setId(userId);
+			userRepository.save(user);
+
+			return ResponseEntity.ok().build();
+
+		} catch (Exception ex) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@DeleteMapping(path = "/delete/{userId}")
+	public ResponseEntity deleteUserById(@PathVariable int userId) {
+		try {
+			userRepository.deleteById(userId);
+			return ResponseEntity.ok().build();
+
+		} catch (Exception ex) {
+			System.out.println("Errorea " + userId + " userra ezabatzerakoan. ");
+			return ResponseEntity.notFound().build();
+
+		}
+	}
 }
