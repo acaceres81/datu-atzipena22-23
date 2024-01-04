87306ba00c7f7cf7702054d258760832327fefda
acaceres81 <114486260+acaceres81@users.noreply.github.com>
2024-01-04 09:20:40 +0100

    projektua pertsonala sortu

---
+++ 4-proiektuPertsonala/proiektuPertsonala/src/main/java/com/Alberto/proiektuPertsonala/ProiektuPertsonalaApplication.java
@@ -0,0 +1,13 @@
+package com.Alberto.proiektuPertsonala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiektuPertsonalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProiektuPertsonalaApplication.class, args);
+	}
+
+}
