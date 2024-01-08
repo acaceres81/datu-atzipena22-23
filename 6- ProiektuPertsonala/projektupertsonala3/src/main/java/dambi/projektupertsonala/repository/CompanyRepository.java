855c60e54ca28f1e386583e3243cea42817998a4
acaceres81 <114486260+acaceres81@users.noreply.github.com>
2024-01-08 01:09:31 +0100

    data as object saved

---
+++ 6- ProiektuPertsonala/projektupertsonala3/src/main/java/dambi/projektupertsonala/repository/CompanyRepository.java
@@ -0,0 +1,15 @@
+package dambi.projektupertsonala.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import dambi.projektupertsonala.model.Company;
+
+public interface CompanyRepository extends MongoRepository<Company, String> {
+    Optional<Company> findByCompany(String company);
+    List<Company> findByDataSalesGreaterThan(double salesValue);
+    List<Company> findByDataRndGreaterThan(double value);
+    List<Company> findByCountryEquals(String countryValue);
+    List<Company> findByDataRnd(double rnd);
+}
